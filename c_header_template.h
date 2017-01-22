169b6300a7d30f7730f0370a2543b2ef7d25b03f
Joe Baltimore <joebalt@skypeak.com>
2017-01-22 18:31:17 -0500

    Create c_header_template.h

---
+++ c_header_template.h
@@ -0,0 +1,19 @@
+/*******************************************
+ * purpose : Template for beginning C header file.
+ * author  : joebalt@skypeak.com
+ * date    : 30-Aug-2016
+ * license : Apache 2.0
+ *******************************************/
+
+#ifndef _THISHEADERNAME_H
+#define _THISHEADERNAME_H
+
+#include <sys/types.h>  /* Type definitions used by many programs */
+#include <stdio.h>      /* Standard I/O functions */
+#include <stdlib.h>     /* Prototypes of commonly used library functions,
+                           plus EXIT_SUCCESS and EXIT_FAILURE constants */
+#include <unistd.h>     /* Prototypes for many system calls */
+#include <errno.h>      /* Declares errno and defines error constants */
+#include <string.h>     /* Commonly used string-handling functions */
+
+#endif /* _THISHEADERNAME_H */
